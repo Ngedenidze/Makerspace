662956a8d3c13c5d25b8a510499b84cce1e3da6f
Nika Gedenidze <97714956+Ngedenidze@users.noreply.github.com>
2025-03-09 22:11:03 -0400

    events page new colors

--- src/components/sections/headingPages/CardInfo/SpecialSlice.js
+++ src/components/sections/headingPages/CardInfo/SpecialSlice.js
@@ -4,9 +4,9 @@ export default function SpecialSlice(props) {
       <Link to={props.link} className="menu-card-link">
         <article className="menu-card">
           <section className="menu-card-content">
-            <h2>{props.weekday}</h2> 
-            <h3>{props.eventName}</h3> 
-            <p><strong>Lineup:</strong> {props.djName}</p> 
+            <time className="weekday">{props.weekday}</time> 
+            <time className="date">{props.eventName}</time> 
+            <h4><strong>Lineup:</strong> {props.djName}</h4> 
             <Link to={props.link} className="special-button" rel="noopener noreferrer">
               View
             </Link>
