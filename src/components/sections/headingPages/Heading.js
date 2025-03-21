77e24ee679f6549ee12284f396c7192975178c91
Nika Gedenidze <ngedenidze@outlook.com>
2025-03-21 01:32:35 -0400

    s

--- src/components/sections/headingPages/Heading.js
+++ src/components/sections/headingPages/Heading.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Navigation from "../../Navigation";
 import Hamburger from "../../reusable/Hamburger/Hamburger";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const apiUrl =
   process.env.NODE_ENV === "production"
@@ -69,7 +69,7 @@ export default function Heading() {
         <section className="bar-info">
           <section className="footer-menu">
             <Link className="footer-links" to="/About">
-              <h3>{t('about_us')}</h3>
+              <h3>{t("about_us")}</h3>
             </Link>
             <a
               className="footer-links"
@@ -77,7 +77,7 @@ export default function Heading() {
               rel="noopener noreferrer"
               href="https://www.google.com/maps/dir//Makerspace,+Lado+Gudiashvili+Square,+Tbilisi+0162,+Georgia/@41.6909958,44.8035254,17z/data=!4m7!4m6!1m1!4e2!1m2!1m1!1s0x40440d9a3542444f:0xc579f4ed091da767!3e0?source=lnms"
             >
-              <h3>{t('location')}</h3>
+              <h3>{t("location")}</h3>
             </a>
           </section>
           <section className="socials">
@@ -159,10 +159,20 @@ export default function Heading() {
               >
                 <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 16 C 3 17.093063 3.9069372 18 5 18 L 7 18 L 12 18 L 12 22 L 8 22 L 8 19 L 7 18 L 6 19 L 6 22 L 8 24 L 12 24 L 12 25 C 12 26.105 12.895 27 14 27 L 25 27 C 26.105 27 27 26.105 27 25 L 27 14 C 27 12.895 26.105 12 25 12 L 18 12 L 18 5 C 18 3.9069372 17.093063 3 16 3 L 5 3 z M 5 5 L 16 5 L 16 12 L 14 12 C 12.895 12 12 12.895 12 14 L 12 16 L 5 16 L 5 5 z M 12 14 L 12 13 C 11.755293 13 11.521351 12.969766 11.291016 12.933594 C 11.314874 12.916254 11.341774 12.902596 11.365234 12.884766 C 12.436415 12.070668 13 10.75101 13 9 L 14 9 L 14 8 L 11 8 L 11 6.5 L 10 6.5 L 10 8 L 7 8 L 7 9 L 12 9 C 12 10.54899 11.563585 11.478941 10.759766 12.089844 C 10.53998 12.25688 10.278088 12.396887 9.9902344 12.517578 C 9.667359 12.357894 9.3640918 12.177141 9.109375 11.962891 C 8.3922951 11.359732 8 10.591752 8 10 L 7 10 C 7 10.997248 7.5736736 11.978924 8.4648438 12.728516 C 8.5238513 12.778149 8.5962189 12.817683 8.6582031 12.865234 C 8.1567671 12.945359 7.6170728 13 7 13 L 7 14 C 8.1153185 14 9.1081165 13.884672 9.9570312 13.605469 C 10.57585 13.850013 11.261979 14 12 14 z M 18.402344 15.976562 L 20.59375 15.976562 L 22.962891 23.023438 L 21.009766 23.023438 L 20.570312 21.474609 L 18.269531 21.474609 L 17.816406 23.023438 L 16.039062 23.023438 L 18.402344 15.976562 z M 19.382812 17.564453 L 18.611328 20.185547 L 20.232422 20.185547 L 19.476562 17.564453 L 19.382812 17.564453 z"></path>
               </svg>
-              <button onClick={() => switchLanguage('en')} className="lang-text">EN</button> <span>/</span>
-<button onClick={() => switchLanguage('ka')} className="lang-text">GE</button> 
+              <button
+                onClick={() => switchLanguage("en")}
+                className="lang-text"
+              >
+                EN
+              </button>{" "}
+              <span>/</span>
+              <button
+                onClick={() => switchLanguage("ka")}
+                className="lang-text"
+              >
+                GE
+              </button>
             </div>
-            
           </section>
 
           {/* Hamburger / Info Box Toggle */}
@@ -173,29 +183,29 @@ export default function Heading() {
                 <>
                   <section className="mobile-menu">
                     <Link className="hover-effect" to="/Djs">
-                      <h1>{t('dj_booking')}</h1>
+                      <h1>{t("dj_booking")}</h1>
                     </Link>
                     <Link className="hover-effect" to="/Gallery">
-                      <h1>{t('gallery')}</h1>
+                      <h1>{t("gallery")}</h1>
                     </Link>
                     <section className="mobile-menu-login">
                       {user ? (
                         <Link className="hover-effect" to="/profile">
-                          <h1>{t('profile')}</h1>
+                          <h1>{t("profile")}</h1>
                           {/* Profile icon SVG */}
                         </Link>
                       ) : (
                         <Link className="hover-effect" to="/login">
-                          <h1>{t('login')}</h1>
+                          <h1>{t("login")}</h1>
                           {/* Login icon SVG */}
                         </Link>
                       )}
                     </section>
                   </section>
                   <section className="footer-menu-mobile">
-                    <h2>{t('company')}</h2>
+                    <h2>{t("company")}</h2>
                     <Link className="footer-links" to="/About">
-                      <h3>{t('about_us')}</h3>
+                      <h3>{t("about_us")}</h3>
                     </Link>
                     <a
                       className="footer-links"
@@ -203,11 +213,10 @@ export default function Heading() {
                       rel="noopener noreferrer"
                       href="https://www.google.com/maps/dir//Makerspace,+Lado+Gudiashvili+Square,+Tbilisi+0162,+Georgia/@41.6909958,44.8035254,17z/data=!4m7!4m6!1m1!4e2!1m2!1m1!1s0x40440d9a3542444f:0xc579f4ed091da767!3e0?source=lnms"
                     >
-                      <h3>{t('location')}</h3>
+                      <h3>{t("location")}</h3>
                     </a>
                   </section>
                   <section className="socials-mobile">
-
                     <a
                       className="hover-effect"
                       href="https://www.instagram.com/makerspace_club/"
@@ -248,19 +257,30 @@ export default function Heading() {
                     </a>
                   </section>
                   <div>
-              <svg
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="25"
-                height="25"
-              >
-                <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 16 C 3 17.093063 3.9069372 18 5 18 L 7 18 L 12 18 L 12 22 L 8 22 L 8 19 L 7 18 L 6 19 L 6 22 L 8 24 L 12 24 L 12 25 C 12 26.105 12.895 27 14 27 L 25 27 C 26.105 27 27 26.105 27 25 L 27 14 C 27 12.895 26.105 12 25 12 L 18 12 L 18 5 C 18 3.9069372 17.093063 3 16 3 L 5 3 z M 5 5 L 16 5 L 16 12 L 14 12 C 12.895 12 12 12.895 12 14 L 12 16 L 5 16 L 5 5 z M 12 14 L 12 13 C 11.755293 13 11.521351 12.969766 11.291016 12.933594 C 11.314874 12.916254 11.341774 12.902596 11.365234 12.884766 C 12.436415 12.070668 13 10.75101 13 9 L 14 9 L 14 8 L 11 8 L 11 6.5 L 10 6.5 L 10 8 L 7 8 L 7 9 L 12 9 C 12 10.54899 11.563585 11.478941 10.759766 12.089844 C 10.53998 12.25688 10.278088 12.396887 9.9902344 12.517578 C 9.667359 12.357894 9.3640918 12.177141 9.109375 11.962891 C 8.3922951 11.359732 8 10.591752 8 10 L 7 10 C 7 10.997248 7.5736736 11.978924 8.4648438 12.728516 C 8.5238513 12.778149 8.5962189 12.817683 8.6582031 12.865234 C 8.1567671 12.945359 7.6170728 13 7 13 L 7 14 C 8.1153185 14 9.1081165 13.884672 9.9570312 13.605469 C 10.57585 13.850013 11.261979 14 12 14 z M 18.402344 15.976562 L 20.59375 15.976562 L 22.962891 23.023438 L 21.009766 23.023438 L 20.570312 21.474609 L 18.269531 21.474609 L 17.816406 23.023438 L 16.039062 23.023438 L 18.402344 15.976562 z M 19.382812 17.564453 L 18.611328 20.185547 L 20.232422 20.185547 L 19.476562 17.564453 L 19.382812 17.564453 z"></path>
-              </svg>
-              <button onClick={() => switchLanguage('en')} className="lang-text">EN</button> <span>/</span>
-<button onClick={() => switchLanguage('ka')} className="lang-text">GE</button> 
-            </div>
+                    <svg
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="25"
+                      height="25"
+                    >
+                      <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 16 C 3 17.093063 3.9069372 18 5 18 L 7 18 L 12 18 L 12 22 L 8 22 L 8 19 L 7 18 L 6 19 L 6 22 L 8 24 L 12 24 L 12 25 C 12 26.105 12.895 27 14 27 L 25 27 C 26.105 27 27 26.105 27 25 L 27 14 C 27 12.895 26.105 12 25 12 L 18 12 L 18 5 C 18 3.9069372 17.093063 3 16 3 L 5 3 z M 5 5 L 16 5 L 16 12 L 14 12 C 12.895 12 12 12.895 12 14 L 12 16 L 5 16 L 5 5 z M 12 14 L 12 13 C 11.755293 13 11.521351 12.969766 11.291016 12.933594 C 11.314874 12.916254 11.341774 12.902596 11.365234 12.884766 C 12.436415 12.070668 13 10.75101 13 9 L 14 9 L 14 8 L 11 8 L 11 6.5 L 10 6.5 L 10 8 L 7 8 L 7 9 L 12 9 C 12 10.54899 11.563585 11.478941 10.759766 12.089844 C 10.53998 12.25688 10.278088 12.396887 9.9902344 12.517578 C 9.667359 12.357894 9.3640918 12.177141 9.109375 11.962891 C 8.3922951 11.359732 8 10.591752 8 10 L 7 10 C 7 10.997248 7.5736736 11.978924 8.4648438 12.728516 C 8.5238513 12.778149 8.5962189 12.817683 8.6582031 12.865234 C 8.1567671 12.945359 7.6170728 13 7 13 L 7 14 C 8.1153185 14 9.1081165 13.884672 9.9570312 13.605469 C 10.57585 13.850013 11.261979 14 12 14 z M 18.402344 15.976562 L 20.59375 15.976562 L 22.962891 23.023438 L 21.009766 23.023438 L 20.570312 21.474609 L 18.269531 21.474609 L 17.816406 23.023438 L 16.039062 23.023438 L 18.402344 15.976562 z M 19.382812 17.564453 L 18.611328 20.185547 L 20.232422 20.185547 L 19.476562 17.564453 L 19.382812 17.564453 z"></path>
+                    </svg>
+                    <button
+                      onClick={() => switchLanguage("en")}
+                      className="lang-text"
+                    >
+                      EN
+                    </button>{" "}
+                    <span>/</span>
+                    <button
+                      onClick={() => switchLanguage("ka")}
+                      className="lang-text"
+                    >
+                      GE
+                    </button>
+                  </div>
                 </>
               </pre>
             </section>
